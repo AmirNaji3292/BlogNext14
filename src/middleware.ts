928cdd34ef02ffef61d66ccf2c17604fb5ad0cd9
amirnaji <amir.najjjiii@gamil.com>
2026-07-21 10:29:41 +0330

    fix nextauth providers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,3 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./lib/auth.config";
-
-export default NextAuth(authConfig).auth;
-
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: [],
 };
