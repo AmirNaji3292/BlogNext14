4cdb49c7174d1a9f143981b7e691167440b7edfd
amirnaji <amir.najjjiii@gamil.com>
2026-07-21 11:31:20 +0330

    fix nextauth providers

--- src/app/compnents/adminPosts/adminPosts.jsx
+++ src/app/compnents/adminPosts/adminPosts.jsx
@@ -1,51 +1,84 @@
-
-
 import styles from "./adminPosts.module.css";
 import Image from "next/image";
 import { deletePost } from "@/lib/action";
+import { connectToDb } from "@/lib/utils";
+import { Post } from "@/lib/models";
+
 
 const getPosts = async () => {
-  const res = await fetch("http://localhost:3000/api/blog", {
-    cache: "no-store",
-  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch posts");
-  }
+  await connectToDb();
 
-  return res.json();
+  const posts = await Post.find()
+    .sort({ createdAt: -1 })
+    .lean();
+
+  return posts;
+
 };
 
+
+
 const AdminPosts = async () => {
+
   const posts = await getPosts();
 
+
   return (
-   <div className={`${styles.container} border-b-2 border-red-500`}>
+    <div className={`${styles.container} border-b-2 border-red-500`}>
+
       <h1>Posts</h1>
 
+
       {posts.map((post) => (
-        <div className={styles.post} key={post._id}>
-          <div  className="flex justify-center items-center ml-4 gap-4">
+
+        <div className={styles.post} key={post._id.toString()}>
+
+          <div className="flex justify-center items-center ml-4 gap-4">
+
             <div className="w-[80px] h-[80px] rounded-full overflow-hidden">
+
               <Image
-              src={post.img || "/noAvatar.png"}
-              alt={post.title}
-              width={50}
-              height={50}
+                src={post.img || "/noAvatar.png"}
+                alt={post.title}
+                width={50}
+                height={50}
                 className="w-full h-full object-cover"
-            />
+              />
+
             </div>
-            <span className={styles.postTitle}>{post.title}</span>
+
+
+            <span className={styles.postTitle}>
+              {post.title}
+            </span>
+
           </div>
 
+
           <form action={deletePost}>
-            <input type="hidden" name="id" value={post._id} />
-            <button className={styles.postButton}>Delete</button>
+
+            <input
+              type="hidden"
+              name="id"
+              value={post._id.toString()}
+            />
+
+            <button className={styles.postButton}>
+              Delete
+            </button>
+
           </form>
+
+
         </div>
+
       ))}
+
+
     </div>
   );
 };
+
 
 export default AdminPosts;
